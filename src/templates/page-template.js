fc28ddaf8067ec1511e8e11dd3deb1ce59539c42
Kristian Sølvberg <kristian@wtw.no>
2020-12-31 08:55:05 +0100

    Initial commit

---
+++ src/templates/page-template.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+export const query = graphql`
+  query($id: ID!) {
+    wpgraphql {
+       page(id: $id) {
+        title
+         content
+       }
+     }
+   }
+ `
+
+const PageTemplate = ({ data }) => {
+     const page = data.wpgraphql.page
+         return (
+           <>
+               <h1 dangerouslySetInnerHTML={{ __html: page.title }} />
+               <div dangerouslySetInnerHTML={{ __html: page.content }} />
+             </>
+         )
+}
+
+export default PageTemplate
